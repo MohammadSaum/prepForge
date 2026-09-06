0c03fe810266c659c04f4f4e4244ae4cb605444c
MohammadSaum <mohdsaum14@gmail.com>
2026-09-06 19:48:17 +0530

    chore: initialize frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"; 
+
+const App = () => {
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path = "/" element = {<h1>PrepForge</h1>} />    
+        </Routes>    
+    </BrowserRouter>
+    
+  )
+}
+
+export default App
