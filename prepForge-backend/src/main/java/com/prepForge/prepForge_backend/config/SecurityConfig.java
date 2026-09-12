d58c39b47e358ffdb663e47dfb640f2c7f81f204
MohammadSaum <mohdsaum14@gmail.com>
2026-09-13 01:34:35 +0530

    fix: allow production frontend origin

--- prepForge-backend/src/main/java/com/prepForge/prepForge_backend/config/SecurityConfig.java
+++ prepForge-backend/src/main/java/com/prepForge/prepForge_backend/config/SecurityConfig.java
@@ -56,7 +56,8 @@ public class SecurityConfig {
 
         configuration.setAllowedOrigins(
                 List.of("http://localhost:5173",
-                        "http://localhost:3000")
+                        "http://localhost:3000",
+                        "https://o-no.onrender.com")
         );
 
         configuration.setAllowedMethods(
