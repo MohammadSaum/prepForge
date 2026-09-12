4e0ed16c0120fbbf0d95b1d12bb10037ee046e21
MohammadSaum <mohdsaum14@gmail.com>
2026-09-13 01:57:58 +0530

    fix: allow deployed frontend origin

--- prepForge-backend/src/main/java/com/prepForge/prepForge_backend/config/SecurityConfig.java
+++ prepForge-backend/src/main/java/com/prepForge/prepForge_backend/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(
                 List.of("http://localhost:5173",
                         "http://localhost:3000",
-                        "https://o-no.onrender.com")
+                        "https://o-no-jse1.onrender.com")
         );
 
         configuration.setAllowedMethods(
